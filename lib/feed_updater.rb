e43b4fca0aa3f5d696485c7e006137d7d3ec3ba9
Ivan Shamatov <status.enable@gmail.com>
2014-02-07 22:25:15 +0400

    adding next_tick updater

--- lib/feed_updater.rb
+++ lib/feed_updater.rb
@@ -1,12 +1,42 @@
 # encoding: utf-8
 module JobFeed
   class FeedUpdater
+
+
     # Get all feeds from redis
     # run update on each
     # on_success, we need to get tokens for each feed
     # and push it in PubSub
-    def run
-      JobFeed.redis.incr 'updater'    
+    def self.run
+      feeds = get_feeds
+      updater = Proc.new do
+        feed = feeds.shift
+        if feed.nil?
+          return
+        else
+          feed = Marshal.load(feed)
+          feed.update
+          if feed.updated?
+            counter = feed.new_entries.count
+            # redis.publish ("push_notification", [feed, counter])
+          end
+          EM.next_tick &updater
+        end
+      end
+      EM.next_tick &updater
+    end
+
+
+
+    def get_feeds
+      keys = redis.keys("feed:*")
+      feeds = redis.mget keys
+    end
+
+
+
+    def self.redis
+      JobFeed.redis
     end
   end
 end
